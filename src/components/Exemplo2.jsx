c50dff6f12420f1ea994cb5c9a690f2dbc0606e0
Maria Vitória <mariavitoriabarreto2@gmail.com>
2025-05-28 22:33:49 -0300

    pronto??

--- src/components/Exemplo2.jsx
+++ src/components/Exemplo2.jsx
@@ -1,1 +1,22 @@
-d
+import { useState, useEffect} from "react";
+
+export function Exemplo2() {
+    const [cep, setCep] = useState("")
+
+    useEffect(() => {
+        console.log(cep)
+    }, [cep])
+
+    return (
+        <section>
+            <h2>Exemplo 2:Buscar endereço</h2>
+
+            <div>
+                <input type="number" placeholder="Digite seu CEP" onChange={(input) => setCep(input.target.value)}/>
+            </div>
+        </section>
+    )
+
+
+
+}
