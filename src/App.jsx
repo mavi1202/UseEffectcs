7dd4faa17a9ec49a8804105f9a65e7c316adeb3b
Maria Vitória <mariavitoriabarreto2@gmail.com>
2025-05-28 22:07:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header";
+
+export function App () {
+  return (
+    <div>
+
+       <Header />
+
+    <main>
+
+     <Exemplo1/>
+    </main>
+    
+    </div>
+
+  )
+}
